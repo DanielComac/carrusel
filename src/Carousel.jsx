642c18483bc4e961c596d3f5adeb2cdcc8c7d79f
DanielComac <danielcomacxbox@gmail.com>
2024-03-13 22:34:33 -0600

    v1

---
+++ src/Carousel.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  return (
+    <div className="carousel">
+      <button className='prev' onClick={handlePrev}>Prev</button>
+      <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <button className='next' onClick={handleNext}>Next</button>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from './Carousel';
+import './App.css'; 
+
+const App = () => {
+  const images = [
+    'https://i0.wp.com/utd.edu.mx/wp-content/uploads/2022/07/LOGO-UTD-NUEVO-2022_solo-01.png?fit=1024%2C387&ssl=1',
+    'https://i0.wp.com/utd.edu.mx/wp-content/uploads/2022/07/ELEMENTOS-GRAFICOS-UTD-2022-05-1.png?resize=1290%2C725&ssl=1',
+    'https://tecolotito.elsiglodedurango.com.mx/cdn-cgi/image/format=auto,width=1024/i/2022/03/1054276.jpeg',
+  ];
+
+  return (
+    <div className="app">
+      <h1 className='title'>Carrusel de imágenes</h1>
+      <Carousel images={images} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
